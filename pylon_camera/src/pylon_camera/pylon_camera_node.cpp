82ca39a46d2163b717bba1c4853163f3199711ca
Matej Petrlik <mrs@uavblue.cz>
2021-07-08 17:03:33 +0200

    software restart instead of shutdown when could not grab image

--- pylon_camera/src/pylon_camera/pylon_camera_node.cpp
+++ pylon_camera/src/pylon_camera/pylon_camera_node.cpp
@@ -699,8 +699,24 @@ bool PylonCameraNode::grabImage()
     boost::lock_guard<boost::recursive_mutex> lock(grab_mutex_); 
     if ( !pylon_camera_->grab(img_raw_msg_.data) )
     {
-        ROS_ERROR("[%s]: Could not grab image. Shutting down to respawn.", ros::this_node::getName().c_str());
-        ros::shutdown();
+        /* ROS_ERROR("[%s]: Could not grab image. Shutting down to respawn.", ros::this_node::getName().c_str()); */
+        /* ros::shutdown(); */
+        ROS_ERROR("[%s]: Could not grab image. Trying to reset.", ros::this_node::getName().c_str());
+        cm_status.status_id = dnb_msgs::ComponentStatus::ERROR;
+        cm_status.status_msg = "Could not grab image, trying to reset";
+        if (pylon_camera_parameter_set_.enable_status_publisher_)
+        {
+          componentStatusPublisher.publish(cm_status);
+        }
+        delete pylon_camera_;
+        pylon_camera_ = nullptr;
+        for ( ros::ServiceServer& user_output_srv : set_user_output_srvs_ )
+        {
+            user_output_srv.shutdown();
+        }
+        set_user_output_srvs_.clear();
+        ros::Duration(0.5).sleep();  // sleep for half a second
+        init();
         return false;
     }
     img_raw_msg_.header.stamp = ros::Time::now(); 
